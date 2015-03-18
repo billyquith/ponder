76ec9edd9be4fa15360187d59967947d7edbd87f
Ben Morgan <bmorgan.warwick@gmail.com>
2015-03-18 16:24:46 +0000

    Enable Qt tests to be run under CTest
    Wrap CAMP includes in qt testing with #ifdefs against Q_MOC_RUN.
    This is needed to workaround issues with mixing Qt4 moc and newer
    Boost libraries, as reported here:
    
    https://bugreports.qt.io/browse/QTBUG-22829
    
    Remove un-needed CMake link_directories command as targets are linked
    directly.
    
    Add camptest-qt using add_test so it can be run as part of the `test`
    target.

--- test/qt/propertymapping.hpp
+++ test/qt/propertymapping.hpp
@@ -13,10 +13,10 @@
 ** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 ** copies of the Software, and to permit persons to whom the Software is
 ** furnished to do so, subject to the following conditions:
-** 
+**
 ** The above copyright notice and this permission notice shall be included in
 ** all copies or substantial portions of the Software.
-** 
+**
 ** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 ** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,9 +30,11 @@
 #ifndef CAMPTEST_QT_PROPERTYMAPPING_HPP
 #define CAMPTEST_QT_PROPERTYMAPPING_HPP
 
+#ifndef Q_MOC_RUN
 #include <camp/camptype.hpp>
 #include <camp/class.hpp>
 #include <camp/qt/qt.hpp>
+#endif
 #include <QObject>
 #include <QString>
 #include <iostream>
