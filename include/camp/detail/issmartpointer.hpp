8ac9f99d191905e6c0b74a9f4bd5b8be4a061893
BQ <chinbillybilbo@gmail.com>
2015-09-22 15:16:41 +0100

    Simplify IsSmartPtr.
    From: Commit: 4bdf63d83363047a86dc9601ef182d5d93f22b69 [4bdf63d]
    Parents: c031bba9c4
    Author: Christian Ofenberg <cofenberg@gmail.com>
    Date: 7 May 2015 13:17:20 BST
    Commit Date: 7 May 2015 13:49:24 BST

--- include/camp/detail/issmartpointer.hpp
+++ include/camp/detail/issmartpointer.hpp
@@ -75,18 +75,7 @@ namespace detail
 template <typename T, typename U>
 struct IsSmartPointer
 {
-    // Check non-const signature
-    template <typename V, U* (V::*)()> struct TestForMember {};
-    template <typename V> static TypeYes check(TestForMember<V, &V::operator-> >*);
-    template <typename V> static TypeNo  check(...);
-
-    // Check const signature
-    template <typename V, U* (V::*)() const> struct TestForConstMember {};
-    template <typename V> static TypeYes checkConst(TestForConstMember<V, &V::operator-> >*);
-    template <typename V> static TypeNo  checkConst(...);
-
-    enum {value = (sizeof(check<T>(0)) == sizeof(TypeYes))
-               || (sizeof(checkConst<T>(0)) == sizeof(TypeYes))};
+    enum { value = (!std::is_pointer<T>::value && !std::is_same<T, U>::value) };
 };
 
 } // namespace detail
