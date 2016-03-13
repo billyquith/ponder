03d8e8fbb6dd59b5c41271693ba73436524f0721
BQ <chinbillybilbo@gmail.com>
2016-03-13 15:20:32 +0000

    Script to automate doc update.

---
+++ updocs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Update documentation
+
+DOCDIR=tmpdocs
+
+function clean () {
+    if [ -d $DOCDIR ]; then
+        rm -rf $DOCDIR
+    fi
+}
+
+clean
+
+git clone https://github.com/billyquith/ponder.git $DOCDIR -b master --depth=1
+pushd $DOCDIR
+cmake -G Ninja
+ninja doc
+popd
+
+rm docs/*
+cp $DOCDIR/doc/api/html/* docs/
+
+clean
