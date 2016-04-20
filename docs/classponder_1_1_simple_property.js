3bbfdaccb539d13bd8a6ab502d5efc9612ce8258
BQ <chinbillybilbo@gmail.com>
2016-04-20 10:10:18 +0100

    Update docs

--- docs/classponder_1_1_simple_property.js
+++ docs/classponder_1_1_simple_property.js
@@ -2,5 +2,5 @@ var classponder_1_1_simple_property =
 [
     [ "SimpleProperty", "classponder_1_1_simple_property.html#a7d6222c83b7800c3049ed09171ca1497", null ],
     [ "~SimpleProperty", "classponder_1_1_simple_property.html#a61410cf1c0755d2a505408be082e02ff", null ],
-    [ "accept", "classponder_1_1_simple_property.html#ac603c9659569eaeb657d4a6522cde25c", null ]
+    [ "accept", "classponder_1_1_simple_property.html#a58a15c22d197cef25bdc6f9235c189f5", null ]
 ];
