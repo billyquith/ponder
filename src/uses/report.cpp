c058857b2593493e682e8028a7321b87bc46c7be
BQ <chinbillybilbo@gmail.com>
2016-09-22 16:46:03 +0100

    Reporting.

--- src/uses/report.cpp
+++ src/uses/report.cpp
@@ -30,6 +30,7 @@
 #include <ponder/classvisitor.hpp>
 #include <ponder/detail/format.hpp>
 #include <stack>
+#include <iostream>
 
 #include <ponder/simpleproperty.hpp>
 #include <ponder/arrayproperty.hpp>
@@ -38,16 +39,19 @@
 
 #include <ponder/type.hpp>
 
+#include "json.hpp"
+
 using namespace ponder;
 using ponder::detail::string_view;
 
 namespace {
     
     using namespace ponder::detail::fmt;
+    using json = nlohmann::json;
 
     class Reporter
     {
-    public:
+    public:        
         virtual ~Reporter() {}
         virtual void open(string_view name) {}
         virtual void close() {}
@@ -58,45 +62,86 @@ namespace {
     class TextReporter : public Reporter
     {
         std::stack<string_view> m_what;
-        
+
+        void indent()
+        {
+            int ind = m_what.size();
+            while (ind-- > 0)
+                print("  ");
+        }
+
     public:
+        
         virtual ~TextReporter() {}
         
         void open(string_view name) override
         {
+            indent();
+            print("{}:\n", name);
             m_what.push(name);
-            print("{} [\n", name);
         }
         
         void close() override
         {
-            print("]\n");
             m_what.pop();
         }
         
         void info(string_view key, string_view value) override
         {
-            int ind = m_what.size();
-            while (ind-- > 0)
-                print("  ");
-                
+            indent();
             print("{} = {}\n", key, value);
         }
     };
-    
+
+    class JsonReporter : public Reporter
+    {
+        std::stack<json::object_t> m_what;
+        json::array_t m_root;
+        
+    public:
+        
+        JsonReporter() {}
+        virtual ~JsonReporter() {}
+        
+        std::string toJson()
+        {
+//            json::object_t ret;
+//            ret["classes"] = m_root;
+//            return ret;
+            return "";
+        }
+        
+        void open(string_view name) override
+        {
+            json::object_t node;
+            m_root.push_back(node);
+            m_what.push(node);
+        }
+        
+        void close() override
+        {
+            m_what.pop();
+        }
+        
+        void info(string_view key, string_view value) override
+        {
+            m_what.top()[key.to_string()] = value.to_string();
+        }
+    };
 
     class ReportVisitor : public ponder::ClassVisitor
     {
         Reporter &m_rp;
         
     public:
+        
         ReportVisitor(Reporter &rp) : m_rp(rp) {}
     
         virtual ~ReportVisitor() {}
 
         virtual void visit(const Property& property) override
         {
-            m_rp.open("property");
+            m_rp.open("Property");
             m_rp.info("name", property.name());
             m_rp.info("kind", enumByType<ValueKind>().name(property.kind()));            
             m_rp.close();
@@ -104,7 +149,7 @@ namespace {
 
         virtual void visit(const SimpleProperty& property) override
         {
-            m_rp.open("simple_property");
+            m_rp.open("SimpleProperty");
             m_rp.info("name", property.name());
             m_rp.info("kind", enumByType<ValueKind>().name(property.kind()));
             m_rp.close();
@@ -112,7 +157,7 @@ namespace {
 
         virtual void visit(const ArrayProperty& property) override
         {
-            m_rp.open("array_property");
+            m_rp.open("ArrayProperty");
             m_rp.info("name", property.name());
             m_rp.info("kind", enumByType<ValueKind>().name(property.kind()));
             m_rp.close();
@@ -120,7 +165,7 @@ namespace {
 
         virtual void visit(const EnumProperty& property) override
         {
-            m_rp.open("enum_property");
+            m_rp.open("EnumProperty");
             m_rp.info("name", property.name());
             m_rp.info("kind", enumByType<ValueKind>().name(property.kind()));
             m_rp.close();
@@ -128,7 +173,7 @@ namespace {
 
         virtual void visit(const UserProperty& property) override
         {
-            m_rp.open("user_property");
+            m_rp.open("UserProperty");
             m_rp.info("name", property.name());
             m_rp.info("kind", enumByType<ValueKind>().name(property.kind()));
             m_rp.close();
@@ -136,7 +181,7 @@ namespace {
 
         virtual void visit(const Function& function) override
         {
-            m_rp.open("function");
+            m_rp.open("Function");
             m_rp.info("name", function.name());
             m_rp.info("kind", enumByType<FunctionKind>().name(function.kind()));
             m_rp.close();
@@ -171,19 +216,34 @@ static void declarePonder()
 PONDER_TYPE(ValueKind);
 PONDER_TYPE(FunctionKind);
 
+static void reportTo(Reporter &rep)
+{
+    auto const& clmgr = detail::ClassManager::instance();
+    auto nbClasses = clmgr.count();
+    
+    ReportVisitor repVis(rep);
+    for (auto ci = 0; ci < nbClasses; ++ci)
+    {
+        auto const& cls = clmgr.getByIndex(ci);
+        rep.open("Class");
+        rep.info("name", cls.name());
+        cls.visit(repVis);
+        rep.close();
+    }
+}
+
 void ponder::uses::reportAll()
 {
     declarePonder();
     
-    auto const& clmgr = detail::ClassManager::instance();
-    auto nbClasses = clmgr.count();
-    
-    TextReporter tr;
-    ReportVisitor repVis(tr);
-    
-    for (auto ci = 0; ci < nbClasses; ++ci)
     {
-        auto const& cls = clmgr.getByIndex(ci);
-        cls.visit(repVis);
+        TextReporter rep;
+        reportTo(rep);
+    }
+    
+    {
+        JsonReporter rep;
+        reportTo(rep);
+        std::cout << rep.toJson() << std::endl;
     }
 }
