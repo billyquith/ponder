43e0d45d69f69bf600aca7a9c00b3896724653dd
BQ <chinbillybilbo@gmail.com>
2017-01-07 14:43:51 +0000

    Update copyright notice 2017.

--- test/ponder/dictionary.cpp
+++ test/ponder/dictionary.cpp
@@ -5,7 +5,7 @@
  ** The MIT License (MIT)
  **
  ** Copyright (C) 2009-2010 TECHNOGERMA Systems France and/or its subsidiary(-ies).
- ** Copyright (C) 2015-2016 Billy Quith.
+ ** Copyright (C) 2015-2017 Nick Trout.
  **
  ** Permission is hereby granted, free of charge, to any person obtaining a copy
  ** of this software and associated documentation files (the "Software"), to deal
