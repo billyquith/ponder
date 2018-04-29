ec52e794617b12fe3e0e7add25d45ea9f871bfea
BQ <chinbillybilbo@gmail.com>
2018-04-29 12:28:01 +0100

    Update copyright year.

--- src/util.cpp
+++ src/util.cpp
@@ -5,7 +5,7 @@
  ** The MIT License (MIT)
  **
  ** Copyright (C) 2009-2014 TEGESO/TEGESOFT and/or its subsidiary(-ies) and mother company.
-** Copyright (C) 2015-2017 Nick Trout.
+** Copyright (C) 2015-2018 Nick Trout.
   **
  ** Permission is hereby granted, free of charge, to any person obtaining a copy
  ** of this software and associated documentation files (the "Software"), to deal
