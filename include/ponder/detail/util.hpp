8e108f596c780772a412c61561c51b892638ccf6
BQ <chinbillybilbo@gmail.com>
2018-06-11 21:02:16 +0100

    Add own implementation of make_unique
    - It seems some libraries aren't up to date?

--- include/ponder/detail/util.hpp
+++ include/ponder/detail/util.hpp
@@ -158,6 +158,38 @@ template<> struct make_index_sequence<1> : index_sequence<0> { };
 
 #endif // index_sequence
 
+// make_unique supplied for compilers missing it (e.g. clang 5.0 on Travis Linux).
+// source: http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2013/n3656.htm
+    
+template<class T> struct _Unique_if {
+    typedef std::unique_ptr<T> _Single_object;
+};
+
+template<class T> struct _Unique_if<T[]> {
+    typedef std::unique_ptr<T[]> _Unknown_bound;
+};
+
+template<class T, size_t N> struct _Unique_if<T[N]> {
+    typedef void _Known_bound;
+};
+
+template<class T, class... Args>
+typename _Unique_if<T>::_Single_object
+make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template<class T>
+typename _Unique_if<T>::_Unknown_bound
+make_unique(size_t n) {
+    typedef typename std::remove_extent<T>::type U;
+    return std::unique_ptr<T>(new U[n]());
+}
+
+template<class T, class... Args>
+typename _Unique_if<T>::_Known_bound
+make_unique(Args&&...) = delete;
+    
 // Return true if all args true. Useful for variadic template expansions.
 static inline bool allTrue()
     {return true;}
