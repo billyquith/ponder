5a22a1c388d356518ce67c21a2f7fe52ca5d5d16
BQ <chinbillybilbo@gmail.com>
2018-11-17 12:58:07 +0000

    Notes on the options.

--- doc/api/pages/building.hpp
+++ doc/api/pages/building.hpp
@@ -18,14 +18,34 @@ cmake -G "Unix Makefiles" ..
 make
 ```
 
-There are various options you can pass to cmake to turn on optional features:
+There are various options you can pass to cmake to turn on optional features. These are processed
+in `cmake/Config.cmake`.
 
-- `USES_LUA:BOOL` turns Lua binding support on. See \ref ponder::lua.
+Features:
+
+- `USES_LUA:BOOL` - turns Lua binding support on. See \ref ponder::lua.
+
+Tests:
+
+- `BUILD_TEST` - includes the unit tests. You might use this for reference.
+- `BUILD_TEST_LUA:BOOL` - turns Lua testing on. You might want to build this to ensure your project
+  is set up correctly if `USES_LUA` is on.
+- `BUILD_TEST_EXAMPLES:BOOL` - includes the examples used in the documentation.
+
+Options:
+
+- `BUILD_SHARED_LIBS:BOOL` - builds Ponder as a shared library.
+    - Only really applicable to Windows for making DLLs.
+- `CMAKE_BUILD_TYPE:STRING` - sets the build configuration. This defaults to "RelWithDebInfo".
 
 E.g.
 
 ```
-cmake -G "Unix Makefiles" -D USES_LUA=ON ..  # turn on Lua support
+cmake .. \
+    -G "Unix Makefiles" \
+    -D BUILD_TEST=TRUE      # include unit tests \
+    -D USES_LUA=TRUE        # turn on Lua support \
+    -D BUILD_TEST_LUA=TRUE  # include Lua tests
 ```
 
 
