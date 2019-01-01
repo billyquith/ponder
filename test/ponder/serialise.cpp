6e2c006327a3051ae1442cfa16406148f7b899e2
BQ <chinbillybilbo@gmail.com>
2019-01-01 20:28:18 +0000

    Bump copyright year.
    - Happy New Year! :)

--- test/ponder/serialise.cpp
+++ test/ponder/serialise.cpp
@@ -5,7 +5,7 @@
  **
  ** The MIT License (MIT)
  **
- ** Copyright (C) 2015-2018 Nick Trout.
+ ** Copyright (C) 2015-2019 Nick Trout.
  **
  ** Permission is hereby granted, free of charge, to any person obtaining a copy
  ** of this software and associated documentation files (the "Software"), to deal
